5bb1d896dab22ba4e1015c8e8bc6f27f90e4a637
Regan <regs.co@gmail.com>
2016-06-27 17:07:22 -0400

    Added login functionality

--- index.js
+++ index.js
@@ -2,50 +2,119 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
-
+var wikiLinkify = require('wiki-linkify');
 app.set('view engine', 'hbs');
+var session = require('express-session');
+
+app.use(session({
+  secret: 'theBirdIsTheWord',
+  cookie: {
+    maxAge: 60000000000
+  }
+}));
+
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.get('/login', function(request, response) {
+  response.render('login');
+});
+
+app.post('/login-submit', function(request, response) {
+  var cred = request.body;
+  if (cred.username === 'Matt' && cred.password === 'isawesome') {
+    request.session.user = cred.username;
+    response.redirect(request.session.requestUrl);
+  } else {
+    response.redirect('/login');
+  }
+});
+
 app.get('/', function(request, response) {
-  response.render('home');
+  var nameOfUser = request.session.user;
+  console.log(nameOfUser);
+  response.render('home', {user: nameOfUser});
 });
 
 app.get('/:pageName', function (request, response) {
-  response.render('placeholder', {pageName: request.params.pageName});
-});
-
-app.get('/:pageName/edit', function(request, response) {
-  response.render('edit', {pageName: request.params.pageName});
-});
-
-app.get('/:pageName', function (request, response) {
-  response.render('placeholder', {pageName: request.params.pageName});
-});
-
-app.post('/:pageName/save', function(request, response) {
-  var data = request.body;
   var pagename = request.params.pageName;
   var page = 'pages/' + pagename + '.txt';
+  console.log(pagename);
+  fs.access(page, fs.F_OK, function(err) {
+    if (err) {
+      response.render('placeholder', {pageName: request.params.pageName});
+    } else {
+      fs.readFile(page, function(err, buffer) {
+        var string = buffer.toString();
+        var wikiContent = wikiLinkify(string);
+        if (err) {
+          console.log('Error',err);
+          return;
+        }
+        response.render('show-text.hbs', {
+        contents: wikiContent,
+        pageName: pagename
+        });
+      });
+    }
+  });
+});
 
-  console.log(data, pagename);
-  fs.writeFile(page, data.contents, function(err) {
+app.get('/:pageName/edit', authRequired, function(request, response) {
+  var pageName = request.params.pageName;
+  var page = 'pages/' + pageName + '.txt';
+  fs.readFile(page, function(err, buffer) {
+    if (err) {
+      response.render('edit', {
+        title: 'Edit ' + pageName,
+        pageName: pageName
+      });
+      return;
+    }
+    var string = buffer.toString();
+    console.log(string);
+    response.render('edit', {
+      title: 'Edit ' + pageName,
+      pageName: pageName,
+      contents: string
+    });
+  });
+});
+
+app.post('/:pageName/save', authRequired, function(request, response) {
+  var data = request.body.contents;
+  var pagename = request.params.pageName;
+  var page = 'pages/' + pagename + '.txt';
+  fs.writeFile(page, data, function(err) {
     if (err) {
       console.log("Error", err);
       return;
     }
-    console.log(data.contents);
+    console.log(data);
+    response.redirect('/' + pagename);
   });
+});
 
-  fs.readFile(page, function(err, buffer) {
-    var string = buffer.toString();
+function authRequired(request, response, next) {
+ request.session.requestUrl = request.url;
+ console.log('some label', request.session.requestUrl);
+  if (!request.session.user) {
+    response.redirect('/login');
+    // console.log('No access bitch');
+  } else {
+    next();
+  }
+}
+
+app.get('/AllPages', function(request, response) {
+  fs.readdir('/pages', function(err, pages){
     if (err) {
-      console.log('Error',err);
+      console.log(err);
       return;
+    } else {
+      console.log('is this all pages?', pages);
+      response.render('AllPages.hbs');
     }
-    console.log(string);
-    response.render('show-text', {contents: string});
   });
-
 });
 
 app.listen(8080, function() {
